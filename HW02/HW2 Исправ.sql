62618a69b25e1e9c86f61fbf6f5c4c52faa76d80
erikaa <C:\Users\erikaa.KBE\Documents\GitHub\otus-mssql--erik->
2023-03-20 11:32:15 +0300

    2

--- HW02/HW2 Исправ.sql
+++ HW02/HW2 Исправ.sql
@@ -1,17 +1,12 @@
-
--- ---------------------------------------------------------------------------
--- Задание - написать выборки для получения указанных ниже данных.
--- ---------------------------------------------------------------------------
-
+ 
 USE WideWorldImporters
 
 /*
-1. Все товары, в названии которых есть "urgent" или название начинается с "Animal".
-Вывести: ИД товара (StockItemID), наименование товара (StockItemName).
-Таблицы: Warehouse.StockItems.
+1. 
 */
 
-
+ 
+	use[WideWorldImporters]
 
 SELECT 
 		 StockItemID,
@@ -26,14 +21,11 @@ SELECT
 	GO
 
 /*
-2. Поставщиков (Suppliers), у которых не было сделано ни одного заказа (PurchaseOrders).
-Сделать через JOIN, с подзапросом задание принято не будет.
-Вывести: ИД поставщика (SupplierID), наименование поставщика (SupplierName).
-Таблицы: Purchasing.Suppliers, Purchasing.PurchaseOrders.
-По каким колонкам делать JOIN подумайте самостоятельно.
+2 
 */
 
 
+	use[WideWorldImporters]
 SELECT 
 		 ps.SupplierID,
 		 ps.SupplierName
@@ -44,20 +36,7 @@ SELECT
 		ppo.PurchaseOrderID IS NULL
 
 /*
-3. Заказы (Orders) с ценой товара (UnitPrice) более 100$ 
-либо количеством единиц (Quantity) товара более 20 штук
-и присутствующей датой комплектации всего заказа (PickingCompletedWhen).
-Вывести:
-* OrderID
-* дату заказа (OrderDate) в формате ДД.ММ.ГГГГ
-* название месяца, в котором был сделан заказ
-* номер квартала, в котором был сделан заказ
-* треть года, к которой относится дата заказа (каждая треть по 4 месяца)
-* имя заказчика (Customer)
-Добавьте вариант этого запроса с постраничной выборкой,
-пропустив первую 1000 и отобразив следующие 100 записей.
-Сортировка должна быть по номеру квартала, трети года, дате заказа (везде по возрастанию).
-Таблицы: Sales.Orders, Sales.OrderLines, Sales.Customers.
+3   
 */
 
 SELECT o.OrderID, CONVERT(NVARCHAR, o.OrderDate, 104) AS OrderDate, c.CustomerName, DATENAME(MM, o.OrderDate) AS Mounth, DATEPART(QUARTER, o.OrderDate) AS [Quarter],
@@ -76,20 +55,7 @@ JOIN Sales.Customers AS c ON o.CustomerID = c.CustomerID
 WHERE ol.UnitPrice > 100 or ol.Quantity > 20 and ol.PickingCompletedWhen is not null
 ORDER BY [Quarter], [Quarter1], o.OrderDate OFFSET 1000 rows fetch first 100 rows only
 
-
-/*
-4. Заказы поставщикам (Purchasing.Suppliers),
-которые должны быть исполнены (ExpectedDeliveryDate) в январе 2013 года
-с доставкой "Air Freight" или "Refrigerated Air Freight" (DeliveryMethodName)
-и которые исполнены (IsOrderFinalized).
-Вывести:
-* способ доставки (DeliveryMethodName)
-* дата доставки (ExpectedDeliveryDate)
-* имя поставщика
-* имя контактного лица принимавшего заказ (ContactPerson)
-Таблицы: Purchasing.Suppliers, Purchasing.PurchaseOrders, Application.DeliveryMethods, Application.People.
-*/
-
+ 
 	
 SELECT
 	ppo.PurchaseOrderID,
@@ -109,11 +75,7 @@ SELECT
 
 	GO
 
-/*
-5. Десять последних продаж (по дате продажи) с именем клиента и именем сотрудника,
-который оформил заказ (SalespersonPerson).
-Сделать без подзапросов.
-*/
+ 
 
 SELECT TOP 10
 		 so.OrderID,
@@ -129,9 +91,7 @@ SELECT TOP 10
 	GO
 
 /*
-6. Все ид и имена клиентов и их контактные телефоны,
-которые покупали товар "Chocolate frogs 250g".
-Имя товара смотреть в таблице Warehouse.StockItems.
+6  
 */
 
 	DECLARE @StockItemName nvarchar(100) = 'Chocolate frogs 250g', @StockItemID int;
